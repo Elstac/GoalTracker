de406f7e29a8da1a860705079ffb2b8b1d316c36
Jakub Lotko <1elstac1@gmail.com>
2019-05-25 12:55:50 +0200

    Added MainGoalRepository

---
+++ Infrastructure/Data/MainGoalRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Entities;
+
+namespace Infrastructure.Data
+{
+    public interface IMainGoalRepository:IRepository<MainGoal,int>
+    {
+
+    }
+    class MainGoalRepository : BaseRepository<MainGoal, int>, IMainGoalRepository
+    {
+        public MainGoalRepository(ApplicationContext context, ISpecificationEvaluator specificationEvaluator) : base(context, specificationEvaluator)
+        {
+        }
+    }
+}
