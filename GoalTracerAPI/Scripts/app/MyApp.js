4d1c2549dd640267e4f5e3c05eab91df37778cec
Jakub Lotko <1elstac1@gmail.com>
2019-05-28 19:32:48 +0200

    Add a lot of mess

---
+++ GoalTracerAPI/Scripts/app/MyApp.js
@@ -0,0 +1,66 @@
+﻿var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function ($scope, $http) {
+    $scope.postDependecies = new Array();
+    $scope.addTask = false;
+
+    $scope.getTasks = function () {
+        $http({
+            method: "GET",
+            url: "http://localhost:25965/api/task"
+        }).then(function mySuccess(response) {
+            $scope.apiData = response.data;
+        }, function myError(response) {
+            $scope.apiData = response.statusText;
+        });
+    }
+
+    $scope.getTasks();
+
+    $scope.postHttp = function () {
+        $http({
+            method: "POST",
+            url: "http://localhost:25965/api/task",
+            data: { name: $scope.toAdd.Name, date: $scope.toAdd.Date, dependencyId: $scope.toAdd.DependencyId }
+        }).then(function mySuccess(response) {
+            $scope.getTasks();
+            $scope.endAddingTask();
+        });
+
+        $scope.getTasks();
+    }
+
+    $scope.endAddingTask = function () {
+        $scope.addTask = false;
+        toAdd.Name = "";
+        toAdd.DependencyId = 0;
+        toAdd.Date = null;
+    }
+
+    $scope.completeTask = function (task) {
+        $scope.apiData.forEach(function (t) {
+            if (t.DependencyTasks != null) {
+                t.DependencyTasks.forEach(function (dtask) {
+                    if (dtask.Id === task.Id)
+                        dtask.Completed = task.Completed;
+                });
+            }
+        });
+    }
+
+    $scope.changeDependency = function (task, dependecies) {
+        dependecies.add(task);
+    }
+
+    $scope.checkDepTasks = function (task) {
+        if (task.DependencyTasks == null)
+            return true;
+
+        var notCompleted = task.DependencyTasks.find(function (dtask) {
+            if (!(dtask.Completed))
+                return dtask;
+        });
+
+        return (notCompleted == null);
+    }
+});
---
+++ Infrastructure/Data/TaskRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+
+namespace Infrastructure.Data
+{
+    public interface ITaskRepository: IRepository<Task,int>
+    {
+
+    }
+    class TaskRepository : BaseRepository<Task, int>, ITaskRepository
+    {
+        public TaskRepository(ApplicationContext context, ISpecificationEvaluator specificationEvaluator) : base(context, specificationEvaluator)
+        {
+
+        }
+    }
+}
