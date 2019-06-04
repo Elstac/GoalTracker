605b6bf030fa389881f3b2a172fa426364184ffa
Jakub Lotko <1elstac1@gmail.com>
2019-06-04 14:27:54 +0200

    Main goal repository can returns goals with thier dependencies

--- Infrastructure/Data/MainGoalRepository.cs
+++ Infrastructure/Data/MainGoalRepository.cs
@@ -1,15 +1,23 @@
 ﻿using ApplicationCore.Entities;
+using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace Infrastructure.Data
 {
     public interface IMainGoalRepository:IRepository<MainGoal,int>
     {
-
+        IEnumerable<MainGoal> GetAllGoalsWithDependencies();
     }
     class MainGoalRepository : BaseRepository<MainGoal, int>, IMainGoalRepository
     {
         public MainGoalRepository(ApplicationContext context, ISpecificationEvaluator specificationEvaluator) : base(context, specificationEvaluator)
         {
+
+        }
+
+        public IEnumerable<MainGoal> GetAllGoalsWithDependencies()
+        {
+            return context.Set<MainGoal>().Include(mg => mg.Steps).Include(mg => mg.Targets);
         }
     }
 }
